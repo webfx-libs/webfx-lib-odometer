7087f74c85e2690f4b08ae6231b255ae5f97afcf
Bruno Salmon <dev.salmonb@gmail.com>
2024-03-19 19:22:59 +0000

    Fixed webfx export made too late on deploy

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@
 module webfx.lib.odometer {
 
     // Direct dependencies modules
-    requires java.base;
     requires javafx.base;
     requires javafx.graphics;
     requires webfx.platform.resource;
